43efed235b6b6581f1ce272f0679afd6f5da98ed
kroegerama <1519044+kroegerama@users.noreply.github.com>
2019-09-14 18:27:37 +0200

    add DebugUtils for future debugging sessions of androidx.camera 🙄

---
+++ library/src/main/java/com/kroegerama/kaiteki/bcode/DebugUtils.kt
@@ -0,0 +1,32 @@
+package com.kroegerama.kaiteki.bcode
+
+import android.graphics.*
+import android.media.Image
+import com.google.zxing.PlanarYUVLuminanceSource
+import java.io.ByteArrayOutputStream
+
+internal fun Image.toBitmap(): Bitmap {
+    val yBuffer = planes[0].buffer // Y
+    val uBuffer = planes[1].buffer // U
+    val vBuffer = planes[2].buffer // V
+
+    val ySize = yBuffer.remaining()
+    val uSize = uBuffer.remaining()
+    val vSize = vBuffer.remaining()
+
+    val nv21 = ByteArray(ySize + uSize + vSize)
+
+    //U and V are swapped
+    yBuffer.get(nv21, 0, ySize)
+    vBuffer.get(nv21, ySize, vSize)
+    uBuffer.get(nv21, ySize + vSize, uSize)
+
+    val yuvImage = YuvImage(nv21, ImageFormat.NV21, this.width, this.height, null)
+    val out = ByteArrayOutputStream()
+    yuvImage.compressToJpeg(Rect(0, 0, yuvImage.width, yuvImage.height), 50, out)
+    val imageBytes = out.toByteArray()
+    return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
+}
+
+internal fun PlanarYUVLuminanceSource.toBitmap() =
+    Bitmap.createBitmap(renderThumbnail(), thumbnailWidth, thumbnailHeight, Bitmap.Config.ARGB_8888)
