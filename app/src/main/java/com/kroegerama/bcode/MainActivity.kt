b452839bf22783074d67e6fb26d5898563bd7c1c
kroegerama <1519044+kroegerama@users.noreply.github.com>
2019-09-14 18:27:07 +0200

    downgrade androidx.camera from 1.0.0-alpha05 to 1.0.0-alpha03 to make detection work again

--- app/src/main/java/com/kroegerama/bcode/MainActivity.kt
+++ app/src/main/java/com/kroegerama/bcode/MainActivity.kt
@@ -10,7 +10,7 @@ import kotlinx.android.synthetic.main.ac_main.*
 class MainActivity : BaseFragmentActivity<Navigation>(
     layout = R.layout.ac_main,
     fragmentContainer = R.id.container,
-    startIndex = Navigation.DialogExamples
+    startIndex = Navigation.BarcodeFragment
 ) {
 
     override fun setupGUI() {
